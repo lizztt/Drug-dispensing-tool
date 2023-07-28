dab2896ec6b56f8a290fce1db587cce85dedf90d
aruasa26 <131351492+aruasa26@users.noreply.github.com>
2023-07-28 10:25:29 +0300

    Add files via upload

---
+++ editcompanyadmin.php
@@ -0,0 +1,102 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "drug_dispensing";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $SSN = $_POST['SSN'];
+
+    $query = "SELECT * FROM pharm_comp_admin WHERE SSN = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $SSN);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+
+    if ($row) {
+        ?>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Edit Company Admin</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: black;
+                    color: orange;
+                    margin: 20px;
+                }
+
+                label {
+                    color: orange;
+                    display: block;
+                    margin-bottom: 5px;
+                }
+
+                input[type="text"],
+                input[type="email"],
+                input[type="password"] {
+                    padding: 6px 10px;
+                    border: 1px solid orange;
+                    border-radius: 4px;
+                    color: orange;
+                    background-color: black;
+                    margin-bottom: 10px;
+                    width: 100%;
+                }
+
+                input[type="submit"] {
+                    background-color: orange;
+                    color: black;
+                    border: none;
+                    padding: 6px 10px;
+                    cursor: pointer;
+                    border-radius: 4px;
+                    width: auto;
+                }
+
+                input[type="submit"]:hover {
+                    background-color: #FF8C00;
+                }
+
+                form {
+                    max-width: 400px;
+                    margin: 0 auto;
+                }
+
+                .error-message {
+                    color: red;
+                    margin-top: 10px;
+                }
+            </style>
+        </head>
+        <body>
+            <form action="updatecompanyadmin.php" method="POST">
+                <input type="hidden" name="SSN" value="<?php echo $row['SSN']; ?>">
+                <label for="Fname">First Name:</label>
+                <input type="text" name="Fname" value="<?php echo $row['Fname']; ?>"><br>
+                <label for="Lname">Last Name:</label>
+                <input type="text" name="Lname" value="<?php echo $row['Lname']; ?>"><br>
+                <label for="email_address">Email address:</label>
+                <input type="email" name="email_address" value="<?php echo $row['email_address']; ?>"><br>
+                <label for="phone_number">Phone:</label>
+                <input type="text" name="phone_number" value="<?php echo $row['phone_number']; ?>"><br>
+                <label for="password">Password:</label>
+                <input type="password" name="password" value="<?php echo $row['password']; ?>"><br>
+                <input type="submit" value="Update">
+            </form>
+        </body>
+        </html>
+        <?php
+    } else {
+        echo "<div class='error-message'>Company Admin not found.</div>";
+    }
+
+    
+    exit;
+}
+?>
