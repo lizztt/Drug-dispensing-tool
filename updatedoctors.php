572216849c43bcdefc61efd168878e247cd9eaf1
aruasa26 <131351492+aruasa26@users.noreply.github.com>
2023-07-28 10:37:18 +0300

    Add files via upload

---
+++ updatedoctors.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "drug_dispensing";
+
+
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Retrieve user's SSN and updated information from the submitted form
+    $SSN = $_POST['SSN'];
+    $Fname = $_POST['Fname'];
+    $Lname = $_POST['Lname'];
+    $email_address = $_POST['email_address'];
+    $phone_number = $_POST['phone_number'];
+    $password = $_POST['password'];
+
+    echo "Received data: SSN=$SSN, Fname=$Fname, Lname=$Lname, email_address=$email_address, phone_number=$phone_number, password=$password";
+
+
+    // Update the user's information in the database
+    $query = "UPDATE doctors SET Fname = ?, Lname = ?, email_address = ?, phone_number = ?, Password = ? WHERE SSN = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("sssisi", $Fname, $Lname,  $email_address, $phone_number, $password, $SSN);
+    $stmt->execute();
+
+    
+    header("Location: doctorview.php");
+    exit;
+    
+}
+?>
