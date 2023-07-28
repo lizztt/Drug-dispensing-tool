887bc09cce92926f2a88f772218c68c01c77fb25
aruasa26 <131351492+aruasa26@users.noreply.github.com>
2023-07-28 10:38:22 +0300

    Add files via upload

---
+++ updatesupervisors.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "drug_dispensing";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    $SSN = $_POST['SSN'];
+    $Fname = $_POST['Fname'];
+    $Lname = $_POST['Lname'];
+    $age = $_POST['age'];
+    $email_address = $_POST['email_address'];
+    $phone_number = $_POST['phone_number'];
+    $password = $_POST['password'];
+
+    
+    $query = "UPDATE supervisors SET Fname = ?, Lname = ?, age = ?, email_address = ?, phone_number = ?, password = ? WHERE SSN = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ssssisi", $Fname, $Lname, $age, $email_address, $phone_number, $password, $SSN);
+    $stmt->execute();
+
+   
+    header("Location: viewsupervisordetails.php");
+    exit;
+}
+?>
